454f3123d7bb7714580c0617418220f48bf3a8b8
Aaron Warner <aaronpaulwarner@gmail.com>
2018-07-25 18:46:07 +1200

    add user get and user add function

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify, abort
 from flask_restful import Resource, Api
 import pyodbc
 import configparser
@@ -18,7 +18,7 @@ class Servers(Resource):
         cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=%s;DATABASE=%s;UID=%s;PWD=%s' % (sqlconfig['SQLConfig']['server'], sqlconfig['SQLConfig']['database'], sqlconfig['SQLConfig']['username'], sqlconfig['SQLConfig']['password']))
         cursor = cnxn.cursor()
         query = cursor.execute("""SELECT name, cpu, memory FROM dbo.servers""")
-        return {'servers': [i[0] for i in query.fetchall()]} # Fetches first column that is Employee ID
+        return {'servers': [i[0] for i in query.fetchall()]}
 
 class Servers_Name(Resource):
     def get(self, client_id):
@@ -26,10 +26,37 @@ class Servers_Name(Resource):
         cursor = cnxn.cursor()
         query = cursor.execute("""SELECT name, cpu, memory FROM dbo.servers WHERE clientid = ?""", client_id)
         return {'servers': [dict(zip([column[0] for column in cursor.description], row)) for row in query.fetchall()]}
-        
+
+class UserQuery(Resource):
+    def get(self, username):
+        cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=%s;DATABASE=%s;UID=%s;PWD=%s' % (sqlconfig['SQLConfig']['server'], sqlconfig['SQLConfig']['identitydb'], sqlconfig['SQLConfig']['username'], sqlconfig['SQLConfig']['password']))
+        cursor = cnxn.cursor()
+        query = cursor.execute("""SELECT username, firstname, lastname, emailaddress FROM dbo.users WHERE username = ?""", username)
+        return {'userdetails': [dict(zip([column[0] for column in cursor.description], row)) for row in query.fetchall()]}    
+
+class UserAPI(Resource):    
+    def post(self):
+        import datetime
+        jsoncontent = request.get_json()
+        username = jsoncontent['username']
+        password = jsoncontent['password']
+        firstname = jsoncontent['firstname']
+        lastname = jsoncontent['lastname']
+        emailaddress = jsoncontent['email']
+        i = datetime.datetime.now()
+        datecreated = i.isoformat()
+        if username is None or password is None or firstname is None or lastname is None or emailaddress is None:
+            abort(400) # invalid parameters
+        cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=%s;DATABASE=%s;UID=%s;PWD=%s' % (sqlconfig['SQLConfig']['server'], sqlconfig['SQLConfig']['identitydb'], sqlconfig['SQLConfig']['username'], sqlconfig['SQLConfig']['password']))
+        cursor = cnxn.cursor()   
+        cursor.execute("""INSERT INTO dbo.users (username, firstname, lastname, emailaddress, password, created) VALUES (?, ?, ?, ?, ?, ?)""", username, firstname, lastname, emailaddress, password, datecreated)
+        cursor.commit()
+        return ('User ' + username + ' Added')
 
 api.add_resource(Servers, '/servers') # Route_1
 api.add_resource(Servers_Name, '/servers/<client_id>') # Route_3
+api.add_resource(UserQuery, '/users/<username>') # Route_2
+api.add_resource(UserAPI, '/users') # Route_2
 
 
 if __name__ == '__main__':
